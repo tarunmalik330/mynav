9053ee3e92a428244efc838b3f6ff5e763a27ebf
tarunmalik330 <maliktarun2005@gmail.com>
2023-12-27 10:37:06 +0530

    form submit deploy

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,192 +1,74 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const Homepage = () => {
+  const [name, setname] = useState("");
+  const [lastname, setlastname] = useState("");
+  const [contact, setcontact] = useState("");
+  const [email, setemail] = useState("");
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    // console.log(name + lastname, email, contact);
+    emailjs
+      .sendForm(
+        "service_nlvjld9",
+        "template_l7jjvqj",
+        form.current,
+        "qxtBsAqACVCOH2sxs"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <div className=" container">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, magni
-        non! Deleniti eveniet, aperiam illum veritatis, voluptatibus itaque rem
-        totam officiis est iusto fugit eum quaerat! Veniam id error ratione
-        deserunt fugiat impedit incidunt mollitia delectus necessitatibus ad
-        expedita nobis minus quidem, possimus quis rerum culpa obcaecati eos
-        eum? Veniam quo, neque aperiam repellendus natus corporis optio soluta
-        nam, fugiat mollitia sint labore expedita totam esse est dignissimos!
-        Velit amet accusamus et? Dolor vel eveniet quasi, aut culpa expedita,
-        iure hic laborum facilis commodi est esse quia numquam! Assumenda et
-        fugit nobis nostrum nesciunt reprehenderit suscipit odio, obcaecati unde
-        esse officiis nisi debitis rem earum quaerat aut ab aperiam tenetur
-        delectus quia! Error provident reprehenderit sunt assumenda animi
-        delectus accusantium. Suscipit cum, sunt reiciendis perferendis nisi
-        itaque repellat praesentium eius ullam eligendi porro libero similique,
-        laudantium vero quod. Vel temporibus fugit ex, neque voluptate molestiae
-        sunt dicta reprehenderit mollitia, dolores dolorem repudiandae
-        consequuntur. Ex non libero delectus eligendi ducimus, rerum animi
-        veniam quibusdam. Vero molestias debitis quas iste. Saepe temporibus ab
-        nemo fugit enim. Enim ex facere optio facilis exercitationem et repellat
-        hic repudiandae explicabo voluptas illo, eos eligendi harum veniam omnis
-        vero iusto veritatis repellendus ipsam. Eveniet, nulla error?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, magni
-        non! Deleniti eveniet, aperiam illum veritatis, voluptatibus itaque rem
-        totam officiis est iusto fugit eum quaerat! Veniam id error ratione
-        deserunt fugiat impedit incidunt mollitia delectus necessitatibus ad
-        expedita nobis minus quidem, possimus quis rerum culpa obcaecati eos
-        eum? Veniam quo, neque aperiam repellendus natus corporis optio soluta
-        nam, fugiat mollitia sint labore expedita totam esse est dignissimos!
-        Velit amet accusamus et? Dolor vel eveniet quasi, aut culpa expedita,
-        iure hic laborum facilis commodi est esse quia numquam! Assumenda et
-        fugit nobis nostrum nesciunt reprehenderit suscipit odio, obcaecati unde
-        esse officiis nisi debitis rem earum quaerat aut ab aperiam tenetur
-        delectus quia! Error provident reprehenderit sunt assumenda animi
-        delectus accusantium. Suscipit cum, sunt reiciendis perferendis nisi
-        itaque repellat praesentium eius ullam eligendi porro libero similique,
-        laudantium vero quod. Vel temporibus fugit ex, neque voluptate molestiae
-        sunt dicta reprehenderit mollitia, dolores dolorem repudiandae
-        consequuntur. Ex non libero delectus eligendi ducimus, rerum animi
-        veniam quibusdam. Vero molestias debitis quas iste. Saepe temporibus ab
-        nemo fugit enim. Enim ex facere optio facilis exercitationem et repellat
-        hic repudiandae explicabo voluptas illo, eos eligendi harum veniam omnis
-        vero iusto veritatis repellendus ipsam. Eveniet, nulla error?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, magni
-        non! Deleniti eveniet, aperiam illum veritatis, voluptatibus itaque rem
-        totam officiis est iusto fugit eum quaerat! Veniam id error ratione
-        deserunt fugiat impedit incidunt mollitia delectus necessitatibus ad
-        expedita nobis minus quidem, possimus quis rerum culpa obcaecati eos
-        eum? Veniam quo, neque aperiam repellendus natus corporis optio soluta
-        nam, fugiat mollitia sint labore expedita totam esse est dignissimos!
-        Velit amet accusamus et? Dolor vel eveniet quasi, aut culpa expedita,
-        iure hic laborum facilis commodi est esse quia numquam! Assumenda et
-        fugit nobis nostrum nesciunt reprehenderit suscipit odio, obcaecati unde
-        esse officiis nisi debitis rem earum quaerat aut ab aperiam tenetur
-        delectus quia! Error provident reprehenderit sunt assumenda animi
-        delectus accusantium. Suscipit cum, sunt reiciendis perferendis nisi
-        itaque repellat praesentium eius ullam eligendi porro libero similique,
-        laudantium vero quod. Vel temporibus fugit ex, neque voluptate molestiae
-        sunt dicta reprehenderit mollitia, dolores dolorem repudiandae
-        consequuntur. Ex non libero delectus eligendi ducimus, rerum animi
-        veniam quibusdam. Vero molestias debitis quas iste. Saepe temporibus ab
-        nemo fugit enim. Enim ex facere optio facilis exercitationem et repellat
-        hic repudiandae explicabo voluptas illo, eos eligendi harum veniam omnis
-        vero iusto veritatis repellendus ipsam. Eveniet, nulla error?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, magni
-        non! Deleniti eveniet, aperiam illum veritatis, voluptatibus itaque rem
-        totam officiis est iusto fugit eum quaerat! Veniam id error ratione
-        deserunt fugiat impedit incidunt mollitia delectus necessitatibus ad
-        expedita nobis minus quidem, possimus quis rerum culpa obcaecati eos
-        eum? Veniam quo, neque aperiam repellendus natus corporis optio soluta
-        nam, fugiat mollitia sint labore expedita totam esse est dignissimos!
-        Velit amet accusamus et? Dolor vel eveniet quasi, aut culpa expedita,
-        iure hic laborum facilis commodi est esse quia numquam! Assumenda et
-        fugit nobis nostrum nesciunt reprehenderit suscipit odio, obcaecati unde
-        esse officiis nisi debitis rem earum quaerat aut ab aperiam tenetur
-        delectus quia! Error provident reprehenderit sunt assumenda animi
-        delectus accusantium. Suscipit cum, sunt reiciendis perferendis nisi
-        itaque repellat praesentium eius ullam eligendi porro libero similique,
-        laudantium vero quod. Vel temporibus fugit ex, neque voluptate molestiae
-        sunt dicta reprehenderit mollitia, dolores dolorem repudiandae
-        consequuntur. Ex non libero delectus eligendi ducimus, rerum animi
-        veniam quibusdam. Vero molestias debitis quas iste. Saepe temporibus ab
-        nemo fugit enim. Enim ex facere optio facilis exercitationem et repellat
-        hic repudiandae explicabo voluptas illo, eos eligendi harum veniam omnis
-        vero iusto veritatis repellendus ipsam. Eveniet, nulla error?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, magni
-        non! Deleniti eveniet, aperiam illum veritatis, voluptatibus itaque rem
-        totam officiis est iusto fugit eum quaerat! Veniam id error ratione
-        deserunt fugiat impedit incidunt mollitia delectus necessitatibus ad
-        expedita nobis minus quidem, possimus quis rerum culpa obcaecati eos
-        eum? Veniam quo, neque aperiam repellendus natus corporis optio soluta
-        nam, fugiat mollitia sint labore expedita totam esse est dignissimos!
-        Velit amet accusamus et? Dolor vel eveniet quasi, aut culpa expedita,
-        iure hic laborum facilis commodi est esse quia numquam! Assumenda et
-        fugit nobis nostrum nesciunt reprehenderit suscipit odio, obcaecati unde
-        esse officiis nisi debitis rem earum quaerat aut ab aperiam tenetur
-        delectus quia! Error provident reprehenderit sunt assumenda animi
-        delectus accusantium. Suscipit cum, sunt reiciendis perferendis nisi
-        itaque repellat praesentium eius ullam eligendi porro libero similique,
-        laudantium vero quod. Vel temporibus fugit ex, neque voluptate molestiae
-        sunt dicta reprehenderit mollitia, dolores dolorem repudiandae
-        consequuntur. Ex non libero delectus eligendi ducimus, rerum animi
-        veniam quibusdam. Vero molestias debitis quas iste. Saepe temporibus ab
-        nemo fugit enim. Enim ex facere optio facilis exercitationem et repellat
-        hic repudiandae explicabo voluptas illo, eos eligendi harum veniam omnis
-        vero iusto veritatis repellendus ipsam. Eveniet, nulla error?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, magni
-        non! Deleniti eveniet, aperiam illum veritatis, voluptatibus itaque rem
-        totam officiis est iusto fugit eum quaerat! Veniam id error ratione
-        deserunt fugiat impedit incidunt mollitia delectus necessitatibus ad
-        expedita nobis minus quidem, possimus quis rerum culpa obcaecati eos
-        eum? Veniam quo, neque aperiam repellendus natus corporis optio soluta
-        nam, fugiat mollitia sint labore expedita totam esse est dignissimos!
-        Velit amet accusamus et? Dolor vel eveniet quasi, aut culpa expedita,
-        iure hic laborum facilis commodi est esse quia numquam! Assumenda et
-        fugit nobis nostrum nesciunt reprehenderit suscipit odio, obcaecati unde
-        esse officiis nisi debitis rem earum quaerat aut ab aperiam tenetur
-        delectus quia! Error provident reprehenderit sunt assumenda animi
-        delectus accusantium. Suscipit cum, sunt reiciendis perferendis nisi
-        itaque repellat praesentium eius ullam eligendi porro libero similique,
-        laudantium vero quod. Vel temporibus fugit ex, neque voluptate molestiae
-        sunt dicta reprehenderit mollitia, dolores dolorem repudiandae
-        consequuntur. Ex non libero delectus eligendi ducimus, rerum animi
-        veniam quibusdam. Vero molestias debitis quas iste. Saepe temporibus ab
-        nemo fugit enim. Enim ex facere optio facilis exercitationem et repellat
-        hic repudiandae explicabo voluptas illo, eos eligendi harum veniam omnis
-        vero iusto veritatis repellendus ipsam. Eveniet, nulla error?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, magni
-        non! Deleniti eveniet, aperiam illum veritatis, voluptatibus itaque rem
-        totam officiis est iusto fugit eum quaerat! Veniam id error ratione
-        deserunt fugiat impedit incidunt mollitia delectus necessitatibus ad
-        expedita nobis minus quidem, possimus quis rerum culpa obcaecati eos
-        eum? Veniam quo, neque aperiam repellendus natus corporis optio soluta
-        nam, fugiat mollitia sint labore expedita totam esse est dignissimos!
-        Velit amet accusamus et? Dolor vel eveniet quasi, aut culpa expedita,
-        iure hic laborum facilis commodi est esse quia numquam! Assumenda et
-        fugit nobis nostrum nesciunt reprehenderit suscipit odio, obcaecati unde
-        esse officiis nisi debitis rem earum quaerat aut ab aperiam tenetur
-        delectus quia! Error provident reprehenderit sunt assumenda animi
-        delectus accusantium. Suscipit cum, sunt reiciendis perferendis nisi
-        itaque repellat praesentium eius ullam eligendi porro libero similique,
-        laudantium vero quod. Vel temporibus fugit ex, neque voluptate molestiae
-        sunt dicta reprehenderit mollitia, dolores dolorem repudiandae
-        consequuntur. Ex non libero delectus eligendi ducimus, rerum animi
-        veniam quibusdam. Vero molestias debitis quas iste. Saepe temporibus ab
-        nemo fugit enim. Enim ex facere optio facilis exercitationem et repellat
-        hic repudiandae explicabo voluptas illo, eos eligendi harum veniam omnis
-        vero iusto veritatis repellendus ipsam. Eveniet, nulla error?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, magni
-        non! Deleniti eveniet, aperiam illum veritatis, voluptatibus itaque rem
-        totam officiis est iusto fugit eum quaerat! Veniam id error ratione
-        deserunt fugiat impedit incidunt mollitia delectus necessitatibus ad
-        expedita nobis minus quidem, possimus quis rerum culpa obcaecati eos
-        eum? Veniam quo, neque aperiam repellendus natus corporis optio soluta
-        nam, fugiat mollitia sint labore expedita totam esse est dignissimos!
-        Velit amet accusamus et? Dolor vel eveniet quasi, aut culpa expedita,
-        iure hic laborum facilis commodi est esse quia numquam! Assumenda et
-        fugit nobis nostrum nesciunt reprehenderit suscipit odio, obcaecati unde
-        esse officiis nisi debitis rem earum quaerat aut ab aperiam tenetur
-        delectus quia! Error provident reprehenderit sunt assumenda animi
-        delectus accusantium. Suscipit cum, sunt reiciendis perferendis nisi
-        itaque repellat praesentium eius ullam eligendi porro libero similique,
-        laudantium vero quod. Vel temporibus fugit ex, neque voluptate molestiae
-        sunt dicta reprehenderit mollitia, dolores dolorem repudiandae
-        consequuntur. Ex non libero delectus eligendi ducimus, rerum animi
-        veniam quibusdam. Vero molestias debitis quas iste. Saepe temporibus ab
-        nemo fugit enim. Enim ex facere optio facilis exercitationem et repellat
-        hic repudiandae explicabo voluptas illo, eos eligendi harum veniam omnis
-        vero iusto veritatis repellendus ipsam. Eveniet, nulla error?
-      </p>
+      <form ref={form} onSubmit={sendEmail}>
+        <label> Name</label>
+        <input
+          value={name}
+          onChange={(e) => setname(e.target.value)}
+          type="text"
+          name="user_name"
+          className="w-100"
+        />
+        <label>Last Name</label>
+        <input
+          value={lastname}
+          className="w-100"
+          type="text"
+          lastname="user_lastname"
+          onChange={(e) => setlastname(e.target.value)}
+        />
+        <label> Contact no.</label>
+        <input
+          className="w-100"
+          type="text"
+          value={contact}
+          contact="user_number"
+          onChange={(e) => setcontact(e.target.value)}
+        />
+        <label>Email</label>
+        <input
+          className="w-100"
+          value={email}
+          type="text"
+          email="user_email"
+          onChange={(e) => setemail(e.target.value)}
+        />
+        <label>Message</label>
+        <textarea name="message" className="w-100" />
+        <input
+          type="submit"
+          value="send"
+          className=" bg-danger text-white py-2 px-3 rounded-2 border-0"
+        />
+      </form>
     </div>
   );
 };
